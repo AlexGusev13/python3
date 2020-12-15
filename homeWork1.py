5a7a294deec214b004d4048171afd7f28480c25d
AlexGusev13 <alexey_741@mail.ru>
2020-12-15 03:50:34 +0300

    Практическое задание 1.1

---
+++ homeWork1.py
@@ -0,0 +1,11 @@
+year = 2021
+animal = 'Бык'
+
+print(year)
+print(animal)
+
+year = input ('Введите интересующий год ')
+animal = input('Введите животное, относящееся к указанному году ')
+
+print(f'{animal} - символ {year} года')
+
